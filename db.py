f10ad4ab19bb9e8d71d8188e5a830eaba2f3515f
Mateo-mh <byronmh8@gmail.com>
2023-01-03 03:39:58 -0500

    Agregando todo.db y crando la interfaz grafica

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ root = Tk()
 root.title('Hola Mundo: todo list')
 root.geometry('500x500')
 
+# Crear y conectar base de datos
 conn = sqlite3.connect('todo.db')
 
 c = conn.cursor()
@@ -12,10 +13,27 @@ c = conn.cursor()
 c.execute("""
     CREATE TABLE if not exists todo(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        created_at TIMESTAMP NOT NULL CURRENT_TIMESTAMP,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         description TEXT NOT NULL,
         completed BOOLEAN NOT NULL
     );  
 """)
 
 conn.commit()
+# -------------------------------
+
+# Creando la interfaz grafica
+l = Label(root, text='Tarea')
+l.pack(row=0, column=0)
+
+e = Entry(root, width=40)
+e.grid(row=0, column=1)
+
+btn = Button(root, text='Agregar')
+btn.grid(row=0, column=2)
+
+# Creando el frame
+frame = LabelFrame(root, text='Mis tareas', padx=5, pady=5)
+frame.grid(row=1, column=0, columnspan=3, sticky='nswe', padx=5)
+
+root.mainloop()
